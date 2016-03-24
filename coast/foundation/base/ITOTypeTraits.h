86a29e2407975ca15f45f100b24175795be2a37d
Marcel Huber <marcelhuberfoo@gmail.com>
2016-03-24 08:43:07 +0100

    type_traits selection: allow more --std= settings to be used
    Change-Id: Id61a5b8b46afea543e580d53d6ce30de55e14113

--- coast/foundation/base/ITOTypeTraits.h
+++ coast/foundation/base/ITOTypeTraits.h
@@ -12,10 +12,10 @@
 #if defined(USE_TR1)
 #include <tr1/type_traits>
 namespace boost_or_tr1 = std::tr1;
-#elif defined(USE_STD0X)
+#elif defined(USE_STD0X) || defined(USE_STD11) || defined(USE_STD14) || defined(USE_STD17) || defined(USE_STD1y) || defined(USE_STD1z)
 #include <type_traits>
 namespace boost_or_tr1 = std;
-#else
+#else // USE_STD03
 #include <boost/type_traits.hpp>
 namespace boost_or_tr1 = boost;
 #endif
